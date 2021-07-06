d7aeade0ea9035003011bced521a66283e7ba8d6
Toon Geens <toon.geens@xenit.eu>
2021-07-06 18:52:43 +0200

    Clean up comment

--- contentcloud-abac-pdp/src/main/java/eu/contentcloud/security/abac/pdp/PolicyDecisionComponent.java
+++ contentcloud-abac-pdp/src/main/java/eu/contentcloud/security/abac/pdp/PolicyDecisionComponent.java
@@ -3,7 +3,6 @@ package eu.contentcloud.security.abac.pdp;
 import java.util.concurrent.CompletableFuture;
 
 public interface PolicyDecisionComponent {
-
-    // TODO refactor to use CompletableFuture instead
+    
     CompletableFuture<PolicyDecision> authorize(AuthenticationContext principal, RequestContext requestContext);
 }
