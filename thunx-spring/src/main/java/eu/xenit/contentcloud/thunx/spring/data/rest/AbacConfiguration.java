7d05ffda6b93baf789bf90ccee0952617688f9b2
Paul Warren <paul_c_warren@yahoo.com>
2021-07-22 12:15:19 +0100

    Ensure querydsl predication by all Repositories

--- thunx-spring/src/main/java/eu/xenit/contentcloud/thunx/spring/data/rest/AbacConfiguration.java
+++ thunx-spring/src/main/java/eu/xenit/contentcloud/thunx/spring/data/rest/AbacConfiguration.java
@@ -8,14 +8,18 @@ import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import javax.persistence.EntityManager;
 
+import org.springframework.aop.framework.Advised;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.AbacQuerydslPredicateBuilder;
 import org.springframework.data.querydsl.binding.QuerydslBindingsFactory;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.support.Repositories;
 import org.springframework.data.repository.support.RepositoryInvokerFactory;
 import org.springframework.data.rest.webmvc.config.ResourceMetadataHandlerMethodArgumentResolver;
@@ -86,6 +90,31 @@ public class AbacConfiguration {
                             predicateBuilder,
                             factory
                     );
+                }
+
+                return bean;
+            }
+        };
+    }
+
+    @Bean
+    public BeanPostProcessor ensureQueryDslPredication(ApplicationContext applicationContext) {
+        return new BeanPostProcessor() {
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+
+
+                if (bean instanceof Repository) {
+                    if (bean instanceof QuerydslPredicateExecutor == false) {
+
+                        if (AopUtils.isJdkDynamicProxy(bean)) {
+                            Class<?>[] proxiedInterfaces = ((Advised)bean).getProxiedInterfaces();
+                            if (proxiedInterfaces != null && proxiedInterfaces.length >= 1) {
+                                throw new IllegalStateException(String.format("%s must implement QueryDslPredicateExecutor when using @EnableAbac", proxiedInterfaces[0]));
+                            }
+                        }
+                        throw new IllegalStateException("All repositories must implement QueryDslPredicateExecutor when using @EnableAbac");
+                    }
                 }
 
                 return bean;
