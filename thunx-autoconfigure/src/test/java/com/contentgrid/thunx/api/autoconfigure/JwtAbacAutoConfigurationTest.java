ab56f37ce4613e012b59c85e3b9696c8a55e2615
niels cappelle <niels.cappelle@xenit.eu>
2025-06-04 11:03:20 +0200

    fix spring-cloud-gateway -> spring-cloud-gateway-server-webflux

--- thunx-autoconfigure/src/test/java/com/contentgrid/thunx/api/autoconfigure/JwtAbacAutoConfigurationTest.java
+++ thunx-autoconfigure/src/test/java/com/contentgrid/thunx/api/autoconfigure/JwtAbacAutoConfigurationTest.java
@@ -22,7 +22,7 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 class JwtAbacAutoConfigurationTest {
 
     WebApplicationContextRunner contextRunner = new WebApplicationContextRunner()
-            .withPropertyValues("spring.cloud.gateway.enabled=false")
+            .withPropertyValues("spring.cloud.gateway.server.webflux.enabled=false")
             .withInitializer(ConditionEvaluationReportLoggingListener.forLogLevel(LogLevel.INFO))
             .withConfiguration(AutoConfigurations.of(
                     AbacAutoConfiguration.class, JwtAbacAutoConfiguration.class
