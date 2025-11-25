8b07e087e3ccb676a536fb45d1ed32cce6cfe575
niels cappelle <niels.cappelle@xenit.eu>
2025-11-25 16:11:13 +0100

    ACC-2431 add test

--- thunx-autoconfigure/src/test/java/com/contentgrid/thunx/gateway/autoconfigure/GatewayAutoConfigurationTest.java
+++ thunx-autoconfigure/src/test/java/com/contentgrid/thunx/gateway/autoconfigure/GatewayAutoConfigurationTest.java
@@ -4,17 +4,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 import com.contentgrid.opa.client.OpaClient;
+import com.contentgrid.thunx.api.autoconfigure.AbacAutoConfiguration;
+import com.contentgrid.thunx.api.autoconfigure.AbacContextAutoConfiguration;
+import com.contentgrid.thunx.api.autoconfigure.JwtAbacAutoConfiguration;
 import com.contentgrid.thunx.pdp.PolicyDecisionPointClient;
 import com.contentgrid.thunx.pdp.opa.OpaQueryProvider;
+import com.contentgrid.thunx.spring.data.context.AbacContextSupplier;
 import com.contentgrid.thunx.spring.gateway.filter.AbacGatewayFilterFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.logging.ConditionEvaluationReportLoggingListener;
 import org.springframework.boot.logging.LogLevel;
+import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.runner.ReactiveWebApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.querydsl.binding.QuerydslBindingsFactory;
 import org.springframework.security.authorization.ReactiveAuthorizationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.authorization.AuthorizationContext;
@@ -71,6 +77,23 @@ public class GatewayAutoConfigurationTest {
                     assertThat(context).getBean(PolicyDecisionPointClient.class).isSameAs(context.getBean(TestContextWithBeans.class).pdpClient());
                     assertThat(context).getBean(ReactiveAuthorizationManager.class).isSameAs(context.getBean(TestContextWithBeans.class).reactiveAuthenticationManager());
                     assertThat(context).getBean(AbacGatewayFilterFactory.class).isSameAs(context.getBean(TestContextWithBeans.class).abacGatewayFilterFactory());
+                });
+    }
+
+    @Test
+    public void shouldNotFailWithApiAutoconfiguations() {
+        contextRunner.withUserConfiguration(TestContext.class)
+                .withConfiguration(AutoConfigurations.of(AbacContextAutoConfiguration.class, AbacAutoConfiguration.class, JwtAbacAutoConfiguration.class))
+                .withClassLoader(new FilteredClassLoader(
+                        "com.contentgrid.thunx.spring.data",
+                        "com.contentgrid.thunx.predicates.querydsl"
+                ))
+                .run(context -> {
+                    assertThat(context).hasNotFailed();
+                    assertThat(context).hasSingleBean(OpaProperties.class);
+
+                    assertThat(context).doesNotHaveBean(QuerydslBindingsFactory.class);
+                    assertThat(context).doesNotHaveBean(AbacContextSupplier.class);
                 });
     }
 
